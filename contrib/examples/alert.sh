9a409325486167a4680512393a5ccc9cd5af492d
Gary Smith <gary.smith@cit.ie>
2016-02-21 18:08:47 +0000

    nothing much really

--- contrib/examples/alert.sh
+++ contrib/examples/alert.sh
@@ -7,7 +7,7 @@ curl -s -XPOST -H "Content-type: application/json" -H "Authorization: Key demo-k
   "resource": "aac0d57e-4c16-4fd5-bb9b-61aed803a703",
   "event": "HW:NIC:FAILED",
   "group": "Hardware",
-  "severity": "minor",
+  "severity": "debug",
   "environment": "Production",
   "service": [
       "Network"
--- contrib/examples/set-status.sh
+++ contrib/examples/set-status.sh
@@ -3,11 +3,12 @@
 #3f724eb9-ad0c-4eae-9f93-6477e83311b6
 
 
-ENDPOINT=${1:-http://localhost:8080}
+ENDPOINT=${1:-http://localhost:8887}
 
-curl -s -XPOST -H "Content-type: application/json" -H "Authorization: Key demo-key" ${ENDPOINT}/alert/nimbus/3d32cfeb-cca9-4c2f-ab10-f02e899efd20/status -d '
+curl -s -XPOST -H "Content-type: application/json" -H "Authorization: Key demo-key" ${ENDPOINT}/alert/nimbus/70969e9e-fd22-45e2-8877-be4044a5f70f/status -d '
 {
-  "status": "banana"
+  "status": "ack",
+  "text": "status updated to ack by garysmi"
 }'
 echo
 
